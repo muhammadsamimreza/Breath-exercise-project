9bdc50b70832355fb3a77ad51c5a1210ea14662d
muhammadsamimreza <muhammadsamimreza@gmail.com>
2025-08-30 12:53:59 +0600

    just add click function

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const timerDisplay = document.getElementById('timerBox');
+const startBtn = document.getElementById('startBtn');
+const stopBtn = document.getElementById('stopBtn');
+
+function l (l) {
+    console.log(l)
+}
+
+const startTimer = () =>{
+  setInterval(()=>{
+    
+  }, 1000)
+}
+
+
+
+
+
+
+
+
+startBtn.addEventListener("click", ()=>{
+    startTimer()
+})
+
+stopBtn.addEventListener("click", ()=>{
+    
+})
