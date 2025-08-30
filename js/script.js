3491aff9be1aeb02bf3ff92ae13e0449f25a5cb6
muhammadsamimreza <muhammadsamimreza@gmail.com>
2025-08-30 22:04:53 +0600

    css and functionality finally done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 let userTime;
 let timeIntervel;
+let isRunning = false;
 
 const timerDisplay = document.getElementById("timerBox");
 const startBtn = document.getElementById("startBtn");
@@ -14,25 +15,37 @@ const startTimer = (givenTime) => {
     const minutes = parseInt(givenTime / 60, 10);
     const seconds = parseInt(givenTime % 60, 10);
 
-    const minDisply = minutes < 10 ?`0${minutes}` : minutes;
-    const secDisply = seconds < 10 ?`0${seconds}` : seconds;
+    const minDisply = minutes < 10 ? `0${minutes}` : minutes;
+    const secDisply = seconds < 10 ? `0${seconds}` : seconds;
 
     timerDisplay.textContent = `${minDisply}:${secDisply}`;
 
     if (givenTime-- < 0) {
       clearInterval(timeIntervel);
       timerDisplay.textContent = "Breath Out";
-      setTimeout(()=>{
+      setTimeout(() => {
         timerDisplay.textContent = "Breath in";
         startTimer(userTime);
-      },3000);
-    } 
+      }, 3000);
+    }
   }, 1000);
 };
 
+const stop = () => {
+  clearInterval(timeIntervel);
+  timerDisplay.textContent = "Breath in";
+  isRunning = false;
+};
+
 startBtn.addEventListener("click", () => {
-  userTime = 15;
-  startTimer(userTime);
+  if (!isRunning) {
+    userTime = 15;
+    startTimer(userTime);
+    isRunning = true;
+  }
 });
 
-stopBtn.addEventListener("click", () => {});
+stopBtn.addEventListener("click", () => {
+  stop();
+  isRunning = false;
+});
