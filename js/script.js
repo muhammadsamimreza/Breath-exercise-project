4c467dfe5a6f1a6a59225f63d5dc7820dcfb3a72
muhammadsamimreza <muhammadsamimreza@gmail.com>
2025-08-30 20:18:35 +0600

    Start button and display time functionality done

--- js/script.js
+++ js/script.js
@@ -1,28 +1,38 @@
-const timerDisplay = document.getElementById('timerBox');
-const startBtn = document.getElementById('startBtn');
-const stopBtn = document.getElementById('stopBtn');
+let userTime;
+let timeIntervel;
 
-function l (l) {
-    console.log(l)
+const timerDisplay = document.getElementById("timerBox");
+const startBtn = document.getElementById("startBtn");
+const stopBtn = document.getElementById("stopBtn");
+
+function l(l) {
+  console.log(l);
 }
 
-const startTimer = () =>{
-  setInterval(()=>{
-    
-  }, 1000)
-}
+const startTimer = (givenTime) => {
+  timeIntervel = setInterval(() => {
+    const minutes = parseInt(givenTime / 60, 10);
+    const seconds = parseInt(givenTime % 60, 10);
 
+    const minDisply = minutes < 10 ?`0${minutes}` : minutes;
+    const secDisply = seconds < 10 ?`0${seconds}` : seconds;
 
+    timerDisplay.textContent = `${minDisply}:${secDisply}`;
 
+    if (givenTime-- < 0) {
+      clearInterval(timeIntervel);
+      timerDisplay.textContent = "Breath Out";
+      setTimeout(()=>{
+        timerDisplay.textContent = "Breath in";
+        startTimer(userTime);
+      },3000);
+    } 
+  }, 1000);
+};
 
+startBtn.addEventListener("click", () => {
+  userTime = 15;
+  startTimer(userTime);
+});
 
-
-
-
-startBtn.addEventListener("click", ()=>{
-    startTimer()
-})
-
-stopBtn.addEventListener("click", ()=>{
-    
-})
+stopBtn.addEventListener("click", () => {});
